d944bcb701ecd5878147043f7dfaa6b6bf6aae81
Will Cox <wcox87@Gmail.com>
2020-09-17 22:19:55 -0600

    CodeFirstAssignment
    Added

---
+++ CodeFirstAssignment/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace CodeFirstAssignment
+{
+    class Program
+    {
+        static void Main()
+        {
+            using (var ctx = new SchoolContext())
+            {
+                var stud = new Student() { StudentName = "Bill" };
+
+                ctx.Students.Add(stud);
+                ctx.SaveChanges();
+            }
+        }
+    }
+}
---
+++ CodeFirstAssignment/SchoolContext.cs
@@ -0,0 +1,16 @@
+﻿using System.Data.Entity;
+
+
+namespace CodeFirstAssignment
+{
+    public class SchoolContext : DbContext
+    {
+        public SchoolContext() : base()
+        {
+
+        }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Grade> Grades { get; set; }
+    }
+}
